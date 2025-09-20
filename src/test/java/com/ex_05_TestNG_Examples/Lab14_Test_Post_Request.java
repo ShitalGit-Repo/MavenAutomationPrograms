ce93c41ff13c9a1639a4ce1a2e522869a261d3f4
IML-WKS-Admin <awacharshital94@gmail.com>
2025-09-20 15:35:31 -0500

    20 Sept 2025

---
+++ src/test/java/com/ex_05_TestNG_Examples/Lab14_Test_Post_Request.java
@@ -0,0 +1,14 @@
+package com.ex_05_TestNG_Examples;
+
+import org.testng.annotations.Test;
+
+public class Lab14_Test_Post_Request {
+
+    public void test_post_request1 () {
+        //below is just a normal function. It will not execute.
+    }
+    @Test  //when you add @Test it become test case
+    public void test_post_request2 (){
+        //when you add @Test it become testNG test case and you can execute this.
+    }
+}
